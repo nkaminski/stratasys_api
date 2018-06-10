e61447b076adcc262728864bbc98f8b3ffbf9341
Nash Kaminski <nashkaminski@comcast.net>
2018-06-10 02:45:01 -0500

    Rework parsing logic to resolve bugs, improve compatibility and simplify.
    Leave the extended machine status prefixed with the codename, instead of 'extended'.
    Parses the cassette section correctly.
    Capable of handling arbitrarily deep lists.

--- stratasys.py
+++ stratasys.py
@@ -1,9 +1,8 @@
 import re
-import json
 from struct import *
 import socket
-import time
 import config as cf
+from pprint import pprint
 
 def make_request(sock, req):
     req_struct = pack('64s', req)
@@ -35,64 +34,65 @@ def printer_get_data(h,p=53742):
         s.close()
         return outdat;
 
-def objproc(key, value):
-        if(key == "machineStatus(queue)"):
-            v1 = value[1:-1].split('}\t')
-            ol=[]
-            for i in v1:
-                if(len(i)<1):
-                    continue
-                lst = i.replace('{','').replace('}','').split("\t-")
-                lst = map(lambda each:each.strip("\t"), lst)
-                lst = [x for x in lst if len(x)>0]
-                od = {}
-                for a in lst:
-                    sloc = a.find(" ")
-                    od[a[:sloc]] = a[sloc:].strip().replace('"','')
-                ol.append(od)
-            return ol
-        else:
-            lst = value.replace('{','').replace('}','').split("\t-")
-            lst = [x for x in lst if len(x)>0]
-            od = {}
-            for i in lst:
-                sloc = i.find(" ")
-                od[i[:sloc]] = i[sloc:].strip().replace('"','')
-            return od
-def stratasys_out_proc(stra):
-        if not(stra):
-            return None
-        stra=stra.replace(b'\n',b'').replace(b'\r',b'').split(b';')
-        out_dict={}
-        bracesplit = re.compile(r"(?P<category>machineStatus\([^\s]+\)) (?P<value>\{.*\})")
-        for s in stra:
-            p=bracesplit.search(s.decode("utf-8"))
-            if(p == None):
-                    p1 = s.decode("utf-8").partition(":");
-                    if(p1[2]):
-                        p1 = tuple(map(lambda each:each.strip(), p1))
-                        out_dict[p1[0]]=p1[2]
-            else:
-                    out_dict[p.group('category')] = objproc(p.group('category'),p.group('value'))
-        out_dict['machineStatus(queue)'] = list(filter(lambda x: len(x.keys()) != 0, out_dict['machineStatus(queue)']))
-        return out_dict
+def parse_output(input_str):
+    if not(input_str):
+        return None
+    #Compile regex, handles headers and removes first brace layer
+    stanza_r = re.compile(r"^set (?P<category>machineStatus\([^\s]+\)) \{(?P<value>.*)\}$")
+    #Normalize input: -\t -> \t, strip CR and LF
+    normalized=input_str.replace(b'\n',b'').replace(b'\r',b'').replace(b'\t-',b'\t').decode('ascii')
+    #Split semicolon delimited stanzas
+    stanzas=normalized.split(';')
+    out_dict={}
+    for stanza in stanzas:
+        #Parse the top level of each stanza
+        match=stanza_r.match(stanza)
+        if not match:
+            continue
+        out_dict[match.group('category')] = parse_stanza(match.group('value'))
+    return out_dict
+
+def parse_stanza(stanza):
+    #Compile regex
+    list_r = re.compile(r'\}\s*\{')
+    #Strip leading or trailing tabs
+    norm_stanza=stanza.strip('\t')
+    if(norm_stanza[0] == '{'):
+        #If the stanza starts with a brace, this is a list.
+        output = list()
+        prelist = norm_stanza[1:-1]
+        for item in list_r.split(prelist):
+            output.append(parse_stanza(item))
+    else:
+        #Otherwise, this is a normal dict of items
+        output = dict()
+        for item in norm_stanza.split('\t'):
+            if len(item) == 0:
+                continue
+            key, val = item.split(' ', 1)
+            output[key]=normalize_value(val)
+    return output
+
+def normalize_value(val):
+    decimal_r = re.compile(r'^-?(\d*\.)?\d+$')
+    int_r = re.compile(r'^-?\d+$')
+    val = val.strip('{}"')
+    if int_r.match(val):
+        val=int(val)
+    elif decimal_r.match(val):
+        val=float(val)
+    return val
+
 def output_postproc(indata):
-     name_map = {'paia' : 'mariner', 'lffs' : 'lffs', 'sst1230' : 'mariner', 'solo' : 'solo'}
-     name=name_map[indata['machineStatus(general)']['modelerType']]
-     nameKey="machineStatus("+name+")"
-     indata['machineStatus(extended)'] = indata[nameKey]
-     del indata[nameKey]
-     if indata['machineStatus(general)']['modelerType'] == 'lffs':
-        indata['machineStatus(extended)']['machineName'] = "Fortus"
-     elif indata['machineStatus(general)']['modelerType'] == 'paia':
-        indata['machineStatus(extended)']['machineName'] = "uPrint"
-     elif indata['machineStatus(general)']['modelerType'] == 'mariner':
-        indata['machineStatus(extended)']['machineName'] = "Dimension"
-     elif indata['machineStatus(general)']['modelerType'] == 'solo':
-        indata['machineStatus(extended)']['machineName'] = "Mojo"
-     else:
-        indata['machineStatus(extended)']['machineName'] = "Other"
-     return indata
+    #Mappings from codenames to friendly/actual names
+    friendlyname_map = {'lffs' : 'Fortus', 'paia' : 'uPrint', 'mariner' : 'Dimension' , 'solo' : 'Mojo', 'dimension' : 'Original Dimension'}
+    #Add the modelerName attribute
+    codename=indata['machineStatus(general)']['modelerType']
+    if codename in friendlyname_map:
+        indata['modelerName'] = friendlyname_map[codename]
+    else:
+        indata['modelerName'] = 'Other'
+    return indata
 
 if( __name__ == "__main__"):
-     print (output_postproc(stratasys_out_proc(printer_get_data(cf.printer_ip))))
+     pprint(output_postproc((parse_output(printer_get_data(cf.printer_ip)))))
